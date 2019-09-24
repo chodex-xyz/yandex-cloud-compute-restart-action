13615c3b5ff9409f17ee2d2267722eba0e0d3ca2
Konstantin Chernyshkov <xchode@gmail.com>
2019-09-25 00:03:18 +0500

    Restart stopped instances

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const core = require('@actions/core');
-const wait = require('./wait');
 
+const {Session}      = require('yandex-cloud');
+const { InstanceService } = require('yandex-cloud/api/compute/v1');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    const oauthToken = core.getInput('oauthToken');
+    const folderId = core.getInput('folderId');
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    const session = new Session({ oauthToken });
+    const instanceService = new InstanceService(session);
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
+    let response = await instanceService.list({folderId})
+
+    let restarted = []
+
+    for (let instance of response.instances) {
+      if (instance.status === 4){
+        let result = await instanceService.start({instanceId: instance.id})
+        core.info(result.description, result.id);
+        restarted.push({instance: instance.name})
+      }
+    }
+
+    core.setOutput('restarted', restarted)
+  }
   catch (error) {
     core.setFailed(error.message);
   }
